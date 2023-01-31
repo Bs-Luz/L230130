ef6b0d775fb6260745b3d8bf431af6e979915a4e
chszk <britzside@gmail.com>
2023-01-31 17:07:05 +0900

    C++ 클래스 추가

---
+++ Source/L230130/L230130.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "L230130.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, L230130, "L230130" );
---
+++ Source/L230130/MyGameModeBase.cpp
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyGameModeBase.h"
+#include "MyPlayerController.h"
+#include "MyPawn.h"
+
+AMyGameModeBase::AMyGameModeBase()
+{
+
+	DefaultPawnClass = AMyPawn::StaticClass(); //Meaning 클래스 이름
+	PlayerControllerClass = AMyPlayerController::StaticClass();
+
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(0, 3.0f, FColor::Red, TEXT("Hello World"));
+	}
+}
